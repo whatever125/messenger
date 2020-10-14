0aec68560f62cdf8f1c94cc857087de5cfedb942
Dmitry <kolmakov125@gmail.com>
2020-10-14 20:53:18 +0700

    Добавлен код сервера и клиента

---
+++ server/server.py
@@ -0,0 +1,21 @@
+from socketserver import BaseRequestHandler, ThreadingMixIn, TCPServer
+
+class MyTCPHandler(BaseRequestHandler):
+    def handle(self):
+        # Получение сообщения
+        data = self.request.recv(1024)
+        # Отправка ответа
+        self.request.sendall('Ответ') 
+
+
+class ThreadedTCPServer(ThreadingMixIn, TCPServer): 
+    pass
+
+
+if __name__ == '__main__':
+    # IP и порт сервера
+    host = 'localhost'
+    port = 55321
+    # Многопоточный TCP сервер
+    with ThreadedTCPServer((host, port), MyTCPHandler) as srv:
+        srv.serve_forever()
