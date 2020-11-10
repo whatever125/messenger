bd51a1fda3327ba9988e3b296609ef1f15546996
Лимон <a.makarov423@gmail.com>
2020-11-11 00:53:01 +0700

    Предпоследняя версия
    Чекни,всё ли тебя устраивает. Осталось только забиндить отправку сообщений на enter,что мы сделаем после перевода файлов ui в класс питона. Советую обратить внимание на настройки интерфейса

--- client.py
+++ client.py
@@ -4,8 +4,10 @@ import json
 import threading
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QWidget, QTextEdit
-from PyQt5.QtCore import QMargins, Qt, QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QWidget, QTextEdit, \
+    QColorDialog
+from PyQt5.QtCore import QMargins, Qt, pyqtSignal, QObject
+
 
 class LTextEdit(QTextEdit):
     def keyPressEvent(self, event):
@@ -14,45 +16,86 @@ class LTextEdit(QTextEdit):
         else:
             QTextEdit.keyPressEvent(self, event)
 
+
+class SettingsDialog(QWidget):
+    def __init__(self, main):
+        super().__init__()
+        uic.loadUi('dialog.ui', self)
+        self.main = main
+        self.pushButton.clicked.connect(main.changecolor1)
+        self.pushButton_3.clicked.connect(main.changecolor2)
+        self.pushButton_2.clicked.connect(main.default)
+
+class Communicate(QObject):
+    newMessage = pyqtSignal()
+
+
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('reg.ui', self)
-        x, y = self.size().width() // 4, self.size().height() // 4
-        self.gridLayout.setContentsMargins(QMargins(x, y, x, y))
-        self.bull = True
-        self.response = None
-        widget = QWidget(self)
-        widget.setLayout(self.gridLayout)
-        self.setCentralWidget(widget)
-        self.ip = 'localhost'
-        self.pushButton.clicked.connect(self.auth)
-        self.pushButton_2.clicked.connect(self.reg)
+        try:
+            f = open('messages/settings', 'r').readlines()
+            self.color1 = f[0]
+            self.color2 = f[1]
+        except Exception:
+            self.color1 = '#50c878'
+            self.color2 = '#ffffff'
+        finally:
+            x, y = self.size().width() // 4, self.size().height() // 4
+            self.gridLayout.setContentsMargins(QMargins(x, y, x, y))
+            self.bull = True
+            self.response = None
+            self.c = Communicate()
+            self.c.newMessage.connect(self.shownewmessages)
+            widget = QWidget(self)
+            widget.setLayout(self.gridLayout)
+            self.setCentralWidget(widget)
+            self.ip = 'localhost'
+            self.pushButton.clicked.connect(self.auth)
+            self.pushButton_2.clicked.connect(self.reg)
+            labels = [self.label, self.label_2, self.label_3, self.label_4]
+            buttons = [self.pushButton, self.pushButton_2]
+            for i in labels:
+                i.setStyleSheet(f'color:{self.color1};')
+            for i in buttons:
+                i.setStyleSheet(f"    background-color:{self.color1};\n"
+                                "     border-style: outset;\n"
+                                "     border-width: 2px;\n"
+                                "     border-radius: 10px;\n"
+                                "     border-color: beige;\n"
+                                "     padding: 6px;\n"
+                                f"color: {self.color2};")
 
     def threading_function(self):
         while True:
             try:
                 self.response = self.client_sock.recv(1024)
-                print(json.loads(self.response))
-                data = self.response
-                if 'action' in json.loads(data).keys():
-                    data = json.loads(data)
-                    chat = open(f'messages/{self.login};{data["from"]}', 'a')
-                    chat.write(f'<font color = #50c878>{data["from"]}: <\\font>\n')
-                    new_text = data['message'].split('\n')
-                    for i in new_text:
-                        chat.write(f'<font color = #ffffff>{i}<\\font>\n')
-                    chat.close()
-                    self.openchat()
-            except Exception as E:
+                self.responses.append(self.response)
+                data = json.loads(self.response)
+                if data['action'] == 'message':
+                    self.c.newMessage.emit()
+            except Exception:
                 return None
+
+    def shownewmessages(self):
+        data = self.responses[-1]
+        data = json.loads(data)
+        chat = open(f'messages/{self.login};{data["from"]}', 'a')
+        chat.write(f'<font color = #50c878>{data["from"]}: <\\font>\n')
+        new_text = data['message'].split('\n')
+        for i in new_text:
+            chat.write(f'<font color = #ffffff>{i}<\\font>\n')
+        chat.close()
+        self.openchat()
+
 
     def auth(self):
         try:
             login = self.lineEdit.text()
             password = self.lineEdit_2.text()
             self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_sock.connect((self.ip, 54321))
+            self.client_sock.connect((self.ip, 54322))
             self.client_sock.sendall(bytes(
                 json.dumps(
                     {"action": "authorize", 'user': {'account_name': login, 'password': password}}),
@@ -73,8 +116,6 @@ class MyWidget(QMainWindow):
         try:
             login = self.lineEdit.text()
             password = self.lineEdit_2.text()
-            self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_sock.connect((self.ip, 54321))
             self.client_sock.sendall(bytes(
                 json.dumps(
                     {'action': 'register', 'user': {'account_name': login, 'password': password}}),
@@ -95,11 +136,14 @@ class MyWidget(QMainWindow):
         self.setCentralWidget(widget)
         self.t = threading.Thread(target=self.threading_function)
         self.t.start()
+        self.responses = []
+        self.recolor()
         self.label.setText(self.login)
         self.pushButton.clicked.connect(self.send)
         self.pushButton_5.clicked.connect(self.logout)
         self.pushButton_2.clicked.connect(self.addcontact)
         self.pushButton_3.clicked.connect(self.delcontact)
+        self.pushButton_4.clicked.connect(self.settings)
         self.listWidget.itemSelectionChanged.connect(self.openchat)
         contacts = self.getcontacts()['contacts']
         for i in contacts:
@@ -109,6 +153,18 @@ class MyWidget(QMainWindow):
     def logout(self):
         self.client_sock.close()
         uic.loadUi('reg.ui', self)
+        labels = [self.label, self.label_2, self.label_3, self.label_4]
+        buttons = [self.pushButton, self.pushButton_2]
+        for i in labels:
+            i.setStyleSheet(f'color:{self.color1};')
+        for i in buttons:
+            i.setStyleSheet(f"    background-color:{self.color1};\n"
+                            "     border-style: outset;\n"
+                            "     border-width: 2px;\n"
+                            "     border-radius: 10px;\n"
+                            "     border-color: beige;\n"
+                            "     padding: 6px;\n"
+                            f"color: {self.color2};")
         widget = QWidget(self)
         widget.setLayout(self.gridLayout)
         self.setCentralWidget(widget)
@@ -172,7 +228,7 @@ class MyWidget(QMainWindow):
 
     def addmessage(self, sender, text):
         new_text = text.split('\n')
-        self.textEdit.append(f'<font color = #50c878>{sender}: <\\font>')
+        self.textEdit.append(f'<font color = {self.color1}>{sender}: <\\font>')
         for i in new_text:
             self.textEdit.append(f'<font color = #ffffff>{i}<\\font>')
         chat = open(f'messages/{self.login};{self.label_2.text()}', 'a')
@@ -193,8 +249,8 @@ class MyWidget(QMainWindow):
                 chat = open(f'messages/{self.login};{i["from"]}', 'a')
                 chat.write(f'<font color = #50c878>{i["from"]}: <\\font>\n')
                 new_text = i['message'].split('\n')
-                for i in new_text:
-                    chat.write(f'<font color = #ffffff>{i}<\\font>\n')
+                for j in new_text:
+                    chat.write(f'<font color = #ffffff>{j}<\\font>\n')
                 chat.close()
         except Exception as E:
             print(E)
@@ -228,9 +284,55 @@ class MyWidget(QMainWindow):
         try:
             chat = open(f'messages/{self.login};{name}', 'r').readlines()
             for i in chat:
+                i = i.replace('#50c878', self.color1)
                 self.textEdit.append(i)
         except Exception as E:
             print(E)
+
+    def changecolor1(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.color1 = color.name()
+        f = open('messages/settings', 'w')
+        f.writelines(f'{self.color1}\n{self.color2}')
+        f.close()
+        self.recolor()
+
+    def changecolor2(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.color2 = color.name()
+        f = open('messages/settings', 'w')
+        f.write(f'{self.color1}\n{self.color2}')
+        f.close()
+        self.recolor()
+
+    def default(self):
+        self.color1 = '#50c878'
+        self.color2 = '#ffffff'
+        f = open('messages/settings', 'w')
+        f.write('#50c878\n#ffffff')
+        f.close()
+        self.recolor()
+
+    def recolor(self):
+        labels = [self.label, self.label_2]
+        buttons = [self.pushButton, self.pushButton_2, self.pushButton_3, self.pushButton_4,
+                   self.pushButton_5]
+        for i in labels:
+            i.setStyleSheet(f'color:{self.color1};')
+        for i in buttons:
+            i.setStyleSheet(f"    background-color:{self.color1};\n"
+                            "     border-style: outset;\n"
+                            "     border-width: 2px;\n"
+                            "     border-radius: 10px;\n"
+                            "     border-color: beige;\n"
+                            "     padding: 6px;\n"
+                            f"color: {self.color2};")
+
+    def settings(self):
+        self.second_form = SettingsDialog(self)
+        self.second_form.show()
 
     def resizeEvent(self, event):
         QMainWindow.resizeEvent(self, event)
@@ -244,6 +346,7 @@ class MyWidget(QMainWindow):
                 self.send()
         except Exception as E:
             print(E)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
