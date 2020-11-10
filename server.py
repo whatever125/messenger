03cea4e0124f782ee92f5eeb2174bbc99d93683b
whatever125 <kolmakov125@gmail.com>
2020-11-10 22:03:17 +0700

    ДоработОчка

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import threading
 class Server:
     def __init__(self):
         self.host = 'localhost'
-        self.port = 54321
+        self.port = 54322
         self.socket = None
         self.clients = []
         self.logins = {}
@@ -27,8 +27,8 @@ class Server:
     def mainloop(self, client_socket, address):
         print(f'Client connected: {address}')
         print()
-        self.con = sqlite3.connect("messenger.sql")
-        self.cur = self.con.cursor()
+        con = sqlite3.connect("messenger.sql")
+        cur = con.cursor()
         self.clients.append(client_socket)
         while True:
             try:
@@ -36,21 +36,21 @@ class Server:
                 print(f'Request from {address}:')
                 print(request)
                 if request['action'] == 'check_online':
-                    resp = self.check_online(request, client_socket)
+                    resp = self.check_online(request, client_socket, con, cur)
                 elif request['action'] == 'authorize':
-                    resp = self.authorization(request, client_socket)
+                    resp = self.authorization(request, client_socket, con, cur)
                 elif request['action'] == 'register':
-                    resp = self.registration(request)
+                    resp = self.registration(request, con, cur)
                 elif request['action'] == 'add_contact':
-                    resp = self.add_contact(request, client_socket)
+                    resp = self.add_contact(request, client_socket, con, cur)
                 elif request['action'] == 'del_contact':
-                    resp = self.del_contact(request, client_socket)
+                    resp = self.del_contact(request, client_socket, con, cur)
                 elif request['action'] == 'get_contacts':
-                    resp = self.get_contacts(request, client_socket)
+                    resp = self.get_contacts(request, client_socket, con, cur)
                 elif request['action'] == 'send_message':
-                    resp = self.handle_message(request, client_socket)
+                    resp = self.handle_message(request, client_socket, con, cur)
                 elif request['action'] == 'get_messages':
-                    resp = self.get_messages(request, client_socket)
+                    resp = self.get_messages(request, client_socket, con, cur)
                 else:
                     raise RuntimeError(f'Unknown request: {request["action"]}')
                 print(f'Response to {address}:')
@@ -59,6 +59,7 @@ class Server:
                 client_socket.send(bytes(json.dumps(resp), encoding='utf8'))
             except Exception as e:
                 print(f'Client disconnected: {address}')
+                print(e)
                 print()
                 client_socket.close()
                 self.clients.remove(client_socket)
@@ -68,14 +69,14 @@ class Server:
                     pass
                 break
 
-    def check_online(self, request, client_socket):
-        resp = {'response': 200, 'error': None, 'online': None}
+    def check_online(self, request, client_socket, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None, 'online': None}
         client_login = request['user']['account_name']
         contact_login = request['user_id']
         if not self.check_authorization(client_socket, client_login):
             resp['response'] = 403
             resp['error'] = 'Access denied'
-        elif not self.check_existence(contact_login):
+        elif not self.check_existence(contact_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'No such client: {contact_login}'
         elif contact_login in self.logins.values():
@@ -84,15 +85,15 @@ class Server:
             resp['online'] = False
         return resp
 
-    def authorization(self, request, client_socket):
-        resp = {'response': 200, 'error': None}
+    def authorization(self, request, client_socket, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None}
         client_login = request['user']['account_name']
         client_digest = request['user']['password']
-        if not self.check_existence(client_login):
+        if not self.check_existence(client_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'No such client: {client_login}'
         else:
-            client_hash = self.get_password(client_login)
+            client_hash = self.get_password(client_login, con, cur)
             print(client_hash, client_digest)
             if client_hash != client_digest:
                 resp['response'] = 403
@@ -101,74 +102,74 @@ class Server:
                 self.logins[client_socket] = client_login
         return resp
 
-    def registration(self, request):
-        resp = {'response': 200, 'error': None}
+    def registration(self, request, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None}
         client_login = request['user']['account_name']
         client_digest = request['user']['password']
-        if self.check_existence(client_login):
+        if self.check_existence(client_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'Login is already taken: {client_login}'
         else:
-            self.register(client_login, client_digest)
+            self.register(client_login, client_digest, con, cur)
         return resp
 
-    def add_contact(self, request, client_socket):
-        resp = {'response': 200, 'error': None}
+    def add_contact(self, request, client_socket, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None}
         client_login = request['user']['account_name']
         contact_login = request['user_id']
         if not self.check_authorization(client_socket, client_login):
             resp['response'] = 403
             resp['error'] = 'Access denied'
-        elif not self.check_existence(contact_login):
+        elif not self.check_existence(contact_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'No such client: {contact_login}'
-        elif self.in_contacts(client_login, contact_login):
+        elif self.in_contacts(client_login, contact_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'Client already in contacts: {contact_login}'
         else:
-            self.add_to_contacts(client_login, contact_login)
+            self.add_to_contacts(client_login, contact_login, con, cur)
         return resp
 
-    def del_contact(self, request, client_socket):
-        resp = {'response': 200, 'error': None}
+    def del_contact(self, request, client_socket, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None}
         client_login = request['user']['account_name']
         contact_login = request['user_id']
         if not self.check_authorization(client_socket, client_login):
             resp['response'] = 403
             resp['error'] = 'Access denied'
-        elif not self.check_existence(contact_login):
+        elif not self.check_existence(contact_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'No such client: {contact_login}'
-        elif not self.in_contacts(client_login, contact_login):
+        elif not self.in_contacts(client_login, contact_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'Client not in contacts: {contact_login}'
         else:
-            self.del_from_contacts(client_login, contact_login)
+            self.del_from_contacts(client_login, contact_login, con, cur)
         return resp
 
-    def get_contacts(self, request, client_socket):
-        resp = {'response': 200, 'error': None, 'contacts': []}
+    def get_contacts(self, request, client_socket, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None, 'contacts': []}
         client_login = request['user']['account_name']
         if not self.check_authorization(client_socket, client_login):
             resp['response'] = 403
             resp['error'] = 'Access denied'
-        elif not self.check_existence(client_login):
+        elif not self.check_existence(client_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'No such client: {client_login}'
         else:
-            client_contacts = json.loads(self.get_client_contacts(client_login))
+            client_contacts = json.loads(self.get_client_contacts(client_login, con, cur))
             resp['contacts'] = client_contacts
         return resp
 
-    def handle_message(self, request, client_socket):
-        resp = {'response': 200, 'error': None}
+    def handle_message(self, request, client_socket, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None}
         client_login = request['user']['account_name']
         contact_login = request['to']
         message = {'action': 'message', 'from': client_login, 'message': None}
         if not self.check_authorization(client_socket, client_login):
             resp['response'] = 403
             resp['error'] = 'Access denied'
-        elif not self.check_existence(contact_login):
+        elif not self.check_existence(contact_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'No such client: {contact_login}'
         elif contact_login in self.logins.values():
@@ -179,84 +180,84 @@ class Server:
                     break
         elif contact_login not in self.logins.values():
             message['message'] = request['message']
-            self.add_unread_messages(contact_login, message)
+            self.add_unread_messages(contact_login, message, con, cur)
         return resp
 
-    def get_messages(self, request, client_socket):
-        resp = {'response': 200, 'error': None, 'messages': []}
+    def get_messages(self, request, client_socket, con, cur):
+        resp = {'action': 'response', 'response': 200, 'error': None, 'messages': []}
         client_login = request['user']['account_name']
         if not self.check_authorization(client_socket, client_login):
             resp['response'] = 403
             resp['error'] = 'Access denied'
-        elif not self.check_existence(client_login):
+        elif not self.check_existence(client_login, con, cur):
             resp['response'] = 400
             resp['error'] = f'No such client: {client_login}'
         else:
-            client_contacts = json.loads(self.get_unread_messages(client_login))
+            client_contacts = json.loads(self.get_unread_messages(client_login, con, cur))
             resp['messages'] = client_contacts
         return resp
 
-    def register(self, client_login, client_password):
-        self.cur.execute("""INSERT INTO users(login, password, contacts, messages) 
+    def register(self, client_login, client_password, con, cur):
+        cur.execute("""INSERT INTO users(login, password, contacts, messages) 
                     VALUES(?, ?, '[]', '[]')""", (client_login, client_password))
-        self.con.commit()
+        con.commit()
 
     def check_authorization(self, client_socket, client_login):
         if client_socket not in self.logins.keys():
             return False
         return self.logins[client_socket] == client_login
 
-    def check_existence(self, client_login):
-        return bool(self.cur.execute("""SELECT login FROM users
+    def check_existence(self, client_login, con, cur):
+        return bool(cur.execute("""SELECT login FROM users
                     WHERE login = ?""", (client_login,)).fetchall())
 
-    def get_password(self, client_login):
-        return self.cur.execute("""SELECT password FROM users
+    def get_password(self, client_login, con, cur):
+        return cur.execute("""SELECT password FROM users
                     WHERE login = ?""", (client_login,)).fetchone()[0]
 
-    def in_contacts(self, client_login, contact_login):
-        contacts = json.loads(self.cur.execute("""SELECT contacts FROM users
+    def in_contacts(self, client_login, contact_login, con, cur):
+        contacts = json.loads(cur.execute("""SELECT contacts FROM users
                     WHERE login = ?""", (client_login,)).fetchone()[0])
         return contact_login in contacts
 
-    def add_to_contacts(self, client_login, contact_login):
-        contacts = json.loads(self.cur.execute("""SELECT contacts FROM users
+    def add_to_contacts(self, client_login, contact_login, con, cur):
+        contacts = json.loads(cur.execute("""SELECT contacts FROM users
                     WHERE login = ?""", (client_login,)).fetchone()[0])
         contacts.append(contact_login)
-        self.cur.execute("""UPDATE users
+        cur.execute("""UPDATE users
                     SET contacts = ?
                     WHERE login = ?""", (json.dumps(contacts), client_login))
-        self.con.commit()
+        con.commit()
 
-    def del_from_contacts(self, client_login, contact_login):
-        contacts = json.loads(self.cur.execute("""SELECT contacts FROM users
+    def del_from_contacts(self, client_login, contact_login, con, cur):
+        contacts = json.loads(cur.execute("""SELECT contacts FROM users
                     WHERE login = ?""", (client_login,)).fetchone()[0])
         del contacts[contacts.index(contact_login)]
-        self.cur.execute("""UPDATE users
+        cur.execute("""UPDATE users
                     SET contacts = ?
                     WHERE login = ?""", (json.dumps(contacts), client_login))
-        self.con.commit()
+        con.commit()
 
-    def get_client_contacts(self, client_login):
-        return self.cur.execute("""SELECT contacts FROM users
+    def get_client_contacts(self, client_login, con, cur):
+        return cur.execute("""SELECT contacts FROM users
                     WHERE login = ?""", (client_login,)).fetchone()[0]
 
-    def add_unread_messages(self, client_login, message):
-        messages = json.loads(self.cur.execute("""SELECT messages FROM users
+    def add_unread_messages(self, client_login, message, con, cur):
+        messages = json.loads(cur.execute("""SELECT messages FROM users
                     WHERE login = ?""", (client_login,)).fetchone()[0])
         messages.append(message)
-        self.cur.execute("""UPDATE users
+        cur.execute("""UPDATE users
                     SET messages = ?
                     WHERE login = ?""", (json.dumps(messages), client_login))
-        self.con.commit()
+        con.commit()
 
-    def get_unread_messages(self, client_login):
-        messages = self.cur.execute("""SELECT messages FROM users
+    def get_unread_messages(self, client_login, con, cur):
+        messages = cur.execute("""SELECT messages FROM users
                             WHERE login = ?""", (client_login,)).fetchone()[0]
-        self.cur.execute("""UPDATE users
+        cur.execute("""UPDATE users
                     SET messages = '[]'
                     WHERE login = ?""", (client_login,))
-        self.con.commit()
+        con.commit()
         return messages
 
 
